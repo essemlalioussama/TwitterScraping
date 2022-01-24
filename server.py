0ba2e5187c69ac3b65a7b901497f65e869c87fc0
essemlalioussama <o.essemlali@gmail.com>
2022-01-24 01:22:31 +0100

    Json form api

--- server.py
+++ server.py
@@ -1,17 +1,43 @@
-from tweet import tweetsByTopic,tweetsByCity
+from tweet import tweetsByCity
 from flask import Flask, jsonify, request
+import random
 
 app = Flask(__name__)
 app.config["DEBUG"]=True
 app.config['JSON_AS_ASCII'] = False
-@app.route('/api/tweets/',methods=['GET'])
+
+
+@app.route('/tweets',methods=['POST'])
 def api_topic():
-    if 'topic' in request.args:
-        topic=str(request.args['topic'])
+    request_data = request.get_json()
+    if 'topic' in request_data:
+        topic=str(request_data['topic'])
+        city=str(request_data['city'])
+        limit=int(request_data['limit'])
     else:
-        return "Error : no topic field provided. please specify a one"
-    tweets = tweetsByTopic(topic)
+        return jsonify({"Error" : "no topic field provided. please specify a one"})
+    tweets = tweetsByCity(topic,city,limit)
+    print(len(tweets))
     return jsonify(tweets)
+
+@app.route('/tweets/<city>',methods=['POST'])
+def api_topicByCity(city):
+    request_data = request.get_json()
+    if 'topic' in request_data:
+        topic=str(request_data['topic'])
+        limit=int(request_data['limit'])
+    else:
+        return jsonify({"Error" : "no topic field provided. please specify a one"})
+    tweets = tweetsByCity(topic,city,limit)
+    print(len(tweets))
+    return jsonify(tweets)
+
+@app.route('/numbers',methods=['POST'])
+def api_numbers():
+    request_data = request.get_json()
+    for city in request_data :
+        city["numbers"] = random.randint(20,30)
+    return jsonify(request_data)
 
 if __name__ == '__main__':
     app.run()
--- tweet.py
+++ tweet.py
@@ -6,14 +6,14 @@ def tweetsByTopic(topic):
             tweets_dic.append(tweetsByCity(topic,str(country["city"])))
     return tweets_dic
 
-def tweetsByCity(topic,city=""):
+def tweetsByCity(topic,city="",limit=20):
     twint.output.tweets_list.clear()
     c = twint.Config()
     c.Search = topic
-    c.Limit = 2
-    c.Near=city
+    c.Limit = limit
+    if(city!=""):
+        c.Near=city
     c.Store_object= True
-    c.Geo = "48.856613,2.352222,10000km"
     c.Show_hashtags = False
     c.Hide_output=True
     twint.run.Search(c)
@@ -23,10 +23,12 @@ def tweetsByCity(topic,city=""):
             'username':tweet.username,
             'tweet':tweet.tweet,
             'date':tweet.datestamp+" "+tweet.timestamp,
-            #'geo':tweet.geo,
+            'geo':tweet.geo,
+            'place':tweet.place,
             'ville':tweet.near,
             'hashtags':tweet.hashtags
         })
+    print(len(tweets))
     return tweets
 #c.Lang = "fr"
 # c.Username = "KylieJenner"
@@ -39,3 +41,6 @@ def tweetsByCity(topic,city=""):
 #remove_file_if_existe('tweets.json')
 #c.Output = "tweets.json"
 # Run
+#c.Geo = "33.969199,-6.927303,10km"
+#c.Location=True
+#c.Popular_tweets=True
